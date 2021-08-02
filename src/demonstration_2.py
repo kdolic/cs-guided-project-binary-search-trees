ba127349d560e710f32cf117aefce5e1a0c6476c
Kenan Dolic <kenandolic@Kenans-MacBook-Pro.local>
2021-08-02 13:52:06 -0500

    completed demo 2 for binary search trees

--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -38,6 +38,43 @@ class TreeNode:
         self.left = left
         self.right = right
 
-def is_valid_BST(self, root):
-    # Your code here
+def is_valid_BST(root):
+    validLeft = True
+    validRight = True
 
+    if root.left is not None:
+        validLeft = is_valid_BST(root.left)
+
+        if root.value < root.left.value:
+            return False
+        
+    if root.right is not None:
+        validRight = is_valid_BST(root.right)
+
+        if root.value > root.right.value:
+            return False
+    
+    if validLeft == True and validRight == True:
+        return True
+    else: 
+        return False
+
+
+validRootTree = TreeNode(5)
+validRootTree.left = TreeNode(3)
+validRootTree.right = TreeNode(7)
+
+invalidRootTree = TreeNode(10)
+invalidRootTree.left = TreeNode(2)
+invalidRootTree.right = TreeNode(8)
+invalidRootTree.right.left = TreeNode(6)
+invalidRootTree.right.right = TreeNode(12)
+
+invalidTree2 = TreeNode(20)
+invalidTree2.left = TreeNode(10)
+invalidTree2.left.right = TreeNode(5)
+invalidTree2.right = TreeNode(30)
+
+print(f'1st tree is: {is_valid_BST(validRootTree)}')
+print(f'2nd tree is: {is_valid_BST(invalidRootTree)}')
+print(f'3rd tree is: {is_valid_BST(invalidTree2)}')
